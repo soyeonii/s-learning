bb05547e8fce574f4f812d19e97a3e074b1546a0
w00cheol <kw8384@gmail.com>
2022-09-09 08:11:47 +0900

    CREATE culture_art, review

--- routes/culture_art.js
+++ routes/culture_art.js
@@ -2,7 +2,9 @@ const CultureArt = require("../model/culture_art");
 
 module.exports = {
 
-  // 문화예술회관 필터링 검색 /culture-arts?a=1&b=2 형식으로 a, b값 전달
+  // 문화예술회관 필터링 검색
+  // city(도시코드), district(구코드) 받음
+  // url:port/culture-arts?a=1&b=2 형식으로 a, b값 전달
   search: async (req, res) => {
     try {
       rows = await CultureArt.search(req.query);
